b64ab91e36dae7f4cf172d6d6e53e80cd0f9bbff
Kacper Kuczkowski <kkuczkowski01@qub.ac.uk>
2019-01-13 16:25:42 +0000

    Add new package; binary

---
+++ src/main/java/binary/IntToBinary.java
@@ -0,0 +1,49 @@
+package binary;
+
+import java.util.Scanner;
+
+/**
+ * Demonstration of integer -> binary conversion.
+ * Constraints: 0 < n && n < 256.
+ */
+public class IntToBinary {
+    public static void main(String[] args)
+    {
+        final int[] table = {128, 64, 32, 16, 8, 4, 2, 1};
+
+        System.out.println("Enter integer below");
+
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        final StringBuilder sb = new StringBuilder();
+
+        convertIntToBinary(sb, table, n);
+        System.out.print(sanitizeResult(sb));
+    }
+
+    private static void convertIntToBinary(StringBuilder sb, int[] t, int n)
+    {
+        for (int val : t)
+        {
+            if (n < 0)
+                break;
+
+            int res = n / val;
+
+            if (res == 1)
+            {
+                sb.append("1");
+                n = n - val;
+            }
+            else if (res == 0)
+            {
+                sb.append("0");
+            }
+        }
+    }
+
+    private static String sanitizeResult(StringBuilder sb)
+    {
+        return sb.toString().replaceAll("^0+", "");
+    }
+}
